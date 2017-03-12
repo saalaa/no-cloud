cea55ff8b8d80ddf45a33fb700ab0e941bd27074
Benoit Myard <myardbenoit@gmail.com>
2017-03-12 01:36:58 +0100

    Python2 fixes in minio code

--- no_cloud/remote/minio.py
+++ no_cloud/remote/minio.py
@@ -8,12 +8,12 @@ from .s3 import RemoteStorage as S3RemoteStorage
 
 class RemoteStorage(S3RemoteStorage):
     def check_s3_config(self):
-        super().check_s3_config()
+        super(RemoteStorage, self).check_s3_config()
 
         assert 'endpoint' in self.config, '`endpoint` not found in configuration'
 
     def build_s3_args(self):
-        args, kwargs = super().build_s3_args()
+        args, kwargs = super(RemoteStorage, self).build_s3_args()
 
         if 'endpoint' in self.config:
             kwargs['endpoint_url'] = self.config['endpoint']
